48c94128f9fd5a7d915f021d8721e5403eeeed98
Gabbynts <gabriella.natasya2003@gmail.com>
2023-04-06 16:53:54 +0700

    feat: delete event

--- src/pages/profileAccount/components/History.tsx
+++ src/pages/profileAccount/components/History.tsx
@@ -11,12 +11,11 @@ import HistoryCard from '@/pages/profileAccount/components/HistoryCard';
 function History() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const [historyList, setHistoryList] = useState([]);
+  const [historyList, setHistoryList] = useState<HistoryType[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const token = localStorage.getItem('token');
-      // console.log(token);
       if (!token) {
         window.location.href = '/login';
         return;
@@ -29,15 +28,11 @@ function History() {
       try {
         const meResponse = await axios.get(`${API_BaseUrl}api/user/me`, config);
         const user_id = meResponse.data.data.id;
-        // console.log(user_id);
 
         const url = `${API_BaseUrl}api/event/user/${user_id}`;
         const response = await axios.get(url, config);
 
         setHistoryList(response.data.data);
-        // console.log(response.data.data);
-        // console.log(response.data);
-        // console.log(response.data.data.length);
         setIsAuthenticated(true);
       } catch (error: any) {
         // console.error(error);
@@ -50,6 +45,30 @@ function History() {
     };
     fetchData();
   }, []);
+
+  const handleDeleteEvent = async (eventId: any) => {
+    const token = localStorage.getItem('token');
+
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+
+    try {
+      const response = await axios.delete(
+        `${API_BaseUrl}api/event/${eventId}`,
+        config
+      );
+
+      // display success message to user
+      alert(response.data.message);
+
+      // update historyList state by filtering out deleted event
+      setHistoryList(historyList.filter((history) => history.id !== eventId));
+    } catch (error: any) {
+      // display error message to user
+      alert(error.message);
+    }
+  };
 
   if (!isAuthenticated) {
     return null;
@@ -72,15 +91,22 @@ function History() {
 
           <div className='group flex flex-wrap justify-center gap-x-3 gap-y-12'>
             {historyList.length > 0 ? (
-              historyList.map((historyList: HistoryType) => (
-                <HistoryCard
-                  key={historyList.id}
-                  href={`/withdraw/${historyList.id}`}
-                  id={historyList.id}
-                  title={historyList.judul_event}
-                  imgUrl={historyList.foto_event}
-                  desc={historyList.deskripsi_event}
-                />
+              historyList.map((historyList) => (
+                <div key={historyList.id}>
+                  <HistoryCard
+                    href={`/withdraw/${historyList.id}`}
+                    id={historyList.id}
+                    title={historyList.judul_event}
+                    imgUrl={historyList.foto_event}
+                    desc={historyList.deskripsi_event}
+                  />
+                  <button
+                    color='secondary'
+                    onClick={() => handleDeleteEvent(historyList.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
               ))
             ) : (
               <Typography sizeVariant='c2' colorVariant='primary'>
